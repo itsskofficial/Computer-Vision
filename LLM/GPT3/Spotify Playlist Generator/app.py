dc0db1ecf27313094afe90663a5ebbe168116d6f
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:40:28 +0000

    Jul 7, 2023, 5:10 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -71,7 +71,7 @@ def main():
     parser = argparse.ArgumentParser(description = 'An AI assistant which generates a Spotify playlist from user input text')
     parser.add_argument('--envfile', help = 'A file which contains your OPENAI_API_KEY', default = '../keys.env', type = str)
     args = parser.parse_args()
-    load_dotenv('../keys.env')
+    load_dotenv(args.envfile)
     openai.api_key = os.getenv('OPENAI_API_KEY')
 
     sp = spotipy.Spotify(
