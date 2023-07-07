11029e6d14f43dbba3f2d017767d78a70aa4cb57
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:51:35 +0000

    Jul 7, 2023, 5:21 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -68,7 +68,7 @@ def main():
     assert spotify_user is not None
     spotify_tracks = []
 
-    propmt = input('Hi there, enter a description of the playlist mood you wanna create : ')
+    prompt = input('Hi there, enter a description of the playlist mood you wanna create : ')
     spotify_playlist = sp.user_playlist_create(
         spotify_user,
         public = False,
