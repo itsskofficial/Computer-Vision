a7fcbe215a27020f06b74fd87f1a3e8927c7c704
SK <sarthakkarandikar03@gmail.com>
2023-07-07 12:01:47 +0000

    Jul 7, 2023, 5:31 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -102,7 +102,7 @@ def main():
 
     print('Creating a Spotify playlist for you...\n')
 
-    create_spotify_playlist(sp, songs)
+    create_spotify_playlist(sp, spotify_user, spotu songs)
 
     
 
