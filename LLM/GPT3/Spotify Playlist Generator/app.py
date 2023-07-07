643f9c7cb7ad0c2af823628d89b6f09e8dcdaf76
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:22:42 +0000

    Jul 7, 2023, 2:52 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -17,7 +17,7 @@ def get_json_from_list(text):
     dictionary = []
     text = text.split('\n')
     for item in text:
-        song = item.split('.')[]
+        song = item.split('.')[1].remo
 
         
 messages = [
