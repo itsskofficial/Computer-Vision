04c14c2dca5812d35cea27a015905f6949910a77
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:38:14 +0000

    Jul 7, 2023, 5:08 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -84,3 +84,4 @@ def get_songs_from_prompt(prompt, count = 10):
     answer = response.to_dict()['choices'][0]['message'].to_dict()['content']
     print(answer)
     json_array = get_json_array_from_list(answer)
+    return json_array
