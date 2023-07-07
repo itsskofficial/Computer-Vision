98a0a89db9f6c34d2ed580588c523778edf4fac2
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:15:50 +0000

    Jul 7, 2023, 2:45 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -13,6 +13,7 @@ BOT_INFO = """
 You are a helpful playlist generating assistant. You should generate a list of songs and their artist according to a text prompt.
 """
 
+def 
 messages = [
     {
         'role' : 'system',
