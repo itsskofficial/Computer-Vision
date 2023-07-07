419ac80bd08d8f68b5df7571bfe47d8076f40199
SK <sarthakkarandikar03@gmail.com>
2023-07-07 12:00:27 +0000

    Jul 7, 2023, 5:30 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -47,7 +47,8 @@ def get_songs_from_prompt(prompt = 'happy songs', count = 10):
     json_array = get_json_array_from_list(answer)
     return json_array
 
-def create_spotify_playlist():
+def create_spotify_playlist(songs):
+    spotify_tracks = []
     for song in songs:
         song_name = song['song']
         artist_name = song['artist']
