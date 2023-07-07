45d37bc2ef88ad8bd0301553758d8c1c12daaaef
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:53:44 +0000

    Jul 7, 2023, 5:23 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -70,6 +70,7 @@ def main():
 
     print('Hi there, I will generate a Spotify playlist for you based on any mood\n')
     prompt = input('Enter the description of the playlist mood you wanna create : ')
+    count = int(input('Enter the number of songs you wanna add in your playlist : '))
     
     spotify_playlist = sp.user_playlist_create(
         spotify_user,
