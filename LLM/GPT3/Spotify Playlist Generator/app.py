90d5ed743b6e7edd1ccf44814455171ee03f92a5
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:16:53 +0000

    Jul 7, 2023, 2:46 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -14,7 +14,7 @@ You are a helpful playlist generating assistant. You should generate a list of s
 """
 
 def get_json_from_list(list):
-    list = 
+    list = list.split('\n')
 messages = [
     {
         'role' : 'system',
