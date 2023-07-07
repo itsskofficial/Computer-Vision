958e3703a2f150ab08d2290ead995f2a0ffc7432
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:25:34 +0000

    Jul 7, 2023, 2:55 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -21,6 +21,7 @@ def get_json_from_list(text):
         song = item.split('.')[1].strip()
         artist = item.split('by')[1].strip()
         
+        
 messages = [
     {
         'role' : 'system',
