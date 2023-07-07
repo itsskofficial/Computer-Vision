072d9c99e96f4d8ef48905d5a700c7180e222fa7
SK <sarthakkarandikar03@gmail.com>
2023-07-07 12:08:06 +0000

    Jul 7, 2023, 5:38 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -10,63 +10,6 @@ Desired format : <serial_no>. <song_name> by <artist_name>
 Don't include any introductory or ending lines
 """
 
-def get_json_array_from_list(text):
-    json_array = []
-    text = text.split('\n')
-    for item in text:
-        song = item.split('.')[1].strip().split('by')[0].strip()
-        artist = item.split('.')[1].strip().split('by')[1].strip()
-        json_array.append(
-            {
-                'song' : song,
-                'artist' : artist
-            }
-        )
-    return json_array
-
-def get_songs_from_prompt(prompt = 'happy songs', count = 10):
-
-    messages = [
-        {
-            'role' : 'system',
-            'content' : BOT_INFO
-        },
-        {
-            'role' : 'user',
-            'content' : f'Generate a playlist of {count} songs based on the prompt {prompt}'
-        }
-    ]
-
-    response = openai.ChatCompletion.create(
-        model = 'gpt-3.5-turbo',
-        messages = messages
-    )
-
-    answer = response.to_dict()['choices'][0]['message'].to_dict()['content']
-    print(answer)
-    json_array = get_json_array_from_list(answer)
-    return json_array
-
-def create_spotify_playlist(sp, spotify_user, spotify_playlist, songs):
-    spotify_tracks = []
-    for song in songs:
-        song_name = song['song']
-        artist_name = song['artist']
-        spotify_search_results = sp.search(
-            q = f'{song_name} {artist_name}',
-            type = 'track',
-            limit = 3
-        )
-
-        spotify_track = spotify_search_results['tracks']['items'][0]['id']
-        spotify_tracks.append(spotify_track)
-    
-    sp.user_playlist_add_tracks(
-        spotify_user['id'],
-        spotify_playlist['id'],
-        spotify_tracks
-    )
-
 def main():
     parser = argparse.ArgumentParser(description = 'An AI assistant which generates a Spotify playlist from user input text')
     parser.add_argument('--envfile', help = 'A file which contains your OPENAI_API_KEY', default = '../keys.env', type = str, required = False)
