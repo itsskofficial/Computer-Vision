ed02cbc31c4f2a6d0d8656af174a3a409f6047ca
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:10:47 +0000

    Jul 7, 2023, 2:40 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -6,7 +6,7 @@ from dotenv import *
 parser = argparse.ArgumentParser(description = 'An AI assistant which generates a Spotify playlist from user input text')
 parser.add_argument('--envfile', help = 'A file which contains your OPENAI_API_KEY', required = False, default = '../keys.env', type = str)
 
-load_dotenv('../keys.env')
+load_dotenv())
 
 BOT_INFO = """
 You are a helpful playlist generating assistant. You should generate a list of songs and their artist according to a text prompt
