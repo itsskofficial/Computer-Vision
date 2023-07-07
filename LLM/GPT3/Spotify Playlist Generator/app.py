f2d15e5a70872850aca7d91e767363b8ab6ceb43
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:40:57 +0000

    Jul 7, 2023, 3:10 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -48,3 +48,4 @@ response = openai.ChatCompletion.create(
 answer = response.to_dict()['choices'][0]['message'].to_dict()['content']
 print(answer)
 json_array = get_json_array_from_list(answer)
+print(json_array)
