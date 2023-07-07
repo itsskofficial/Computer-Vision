f345960f36c6154842e0d2a26d535b39bdef886c
SK <sarthakkarandikar03@gmail.com>
2023-07-07 09:15:39 +0000

    Jul 7, 2023, 2:45 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -11,11 +11,6 @@ openai.api_key = os.environ['OPENAI_API_KEY']
 
 BOT_INFO = """
 You are a helpful playlist generating assistant. You should generate a list of songs and their artist according to a text prompt.
-You should return a JSON array where each element follows this format : 
-{
-    'song' : <song_title>,
-    'artist' : <artist_name>
-}
 """
 
 messages = [
