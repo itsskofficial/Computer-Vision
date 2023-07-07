72887fd6a84ca200140d5e495e73ffbd50f5a1ed
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:19:47 +0000

    Jul 7, 2023, 4:49 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -13,7 +13,8 @@ openai.api_key = os.getenv('OPENAI_API_KEY')
 sp = spotipy.Spotify(
     auth_manager = spotipy.SpotifyOAuth(
         client_id = ,
-        client_secret = 
+        client_secret = ,
+        
     )
 )
 BOT_INFO = """
