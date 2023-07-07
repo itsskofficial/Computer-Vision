f8b1dfd4630eddcd63355032cae42746b3b62fb1
SK <sarthakkarandikar03@gmail.com>
2023-07-07 11:50:02 +0000

    Jul 7, 2023, 5:20 PM

--- LLM/GPT3/Spotify Playlist Generator/app.py
+++ LLM/GPT3/Spotify Playlist Generator/app.py
@@ -4,12 +4,6 @@ import spotipy
 import argparse
 from dotenv import *
 
-
-
-
-
-
-
 BOT_INFO = """
 You are a helpful playlist generating assistant. You should generate a list of songs and their artist according to a text prompt.
 Desired format : <serial_no>. <song_name> by <artist_name>
