ea285f113076e70464c1f0b87d5bd9f7b7137411
SK <sarthakkarandikar03@gmail.com>
2023-11-08 07:04:17 +0530

    imgcap init

--- CNN/imgcap/app.py
+++ CNN/imgcap/app.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+if "caption" not in st.session_state:
+    st.session_state.caption = ""
+
+if "image" not in st.session_state:
+    st.session_state.image = None
+
+if "runpage" not in st.session_state:
+    st.session_state.runpage = None
+
+def app():
+    st.title("ImgCap")
+    st.text("Upload any image below to generate a custom caption")
+    image = st.file_uploader(label = "Only .png & .jpg format supported", type = ["png", "jpg"])
+    st.session_state.image = image
