7c432d513cc671106352c229c71ac10b53c24bb7
SK <sarthakkarandikar03@gmail.com>
2023-11-13 17:21:14 +0530

    streamlit image issue

--- Projects/imgcap/app.py
+++ Projects/imgcap/app.py
@@ -4,10 +4,11 @@ import streamlit as st
 def app():
     st.title("ImgCap")
     st.text("Upload any image below to generate a custom caption")
-    image = st.file_uploader(label = "Only .jpg format supported", type = ["jpg"])
+    image = st.file_uploader(label = None, type = ["jpg"])
 
-    with open("image.jpg", "wb") as file :
-        file.write(image.load_buffer())
+    if image :
+        with open("image.jpg", "wb") as file :
+            file.write(image.load_buffer())
 
     if st.button("Submit") :
         st.session_state.runpage = caption.app
