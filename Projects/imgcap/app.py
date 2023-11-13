5f0b73d91667e2c4b407703da600e0416ef07103
SK <sarthakkarandikar03@gmail.com>
2023-11-13 17:24:25 +0530

    label issue

--- Projects/imgcap/app.py
+++ Projects/imgcap/app.py
@@ -4,7 +4,7 @@ import streamlit as st
 def app():
     st.title("ImgCap")
     st.text("Upload any image below to generate a custom caption")
-    image = st.file_uploader(label = "", type = ["jpg"])
+    image = st.file_uploader(label = "Let the magic happen", type = ["jpg"])
 
     if image :
         with open("image.jpg", "wb") as file :
