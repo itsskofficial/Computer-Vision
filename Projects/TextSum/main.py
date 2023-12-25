363818541b18562ffdff048f0591051ae6507964
SK <sarthakkarandikar03@gmail.com>
2023-12-25 16:03:21 +0530

    model evaluation stage completed

--- Projects/TextSum/main.py
+++ Projects/TextSum/main.py
@@ -1,6 +1,8 @@
 from TextSum.pipeline.data_ingestion import DataIngestionTrainingPipeline
 from TextSum.pipeline.data_validation import DataValidationTrainingPipeline
 from TextSum.pipeline.data_transformation import DataTransformationTrainingPipeline
+from TextSum.pipeline.model_trainer import ModelTrainingPipeline
+from TextSum.pipeline.model_evaluation import ModelEvaluationPipeline
 from TextSum.logging import logger
 
 STAGE_NAME = "Data Ingestion"
@@ -33,6 +35,30 @@ try:
     logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
     data_transformation = DataTransformationTrainingPipeline()
     data_transformation.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Training"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Evaluation"
+
+try:
+    logger.info(f">>>>>> {STAGE_NAME} started <<<<<<")
+    model_evaluation = ModelEvaluationPipeline()
+    model_evaluation.main()
     logger.info(f">>>>>> {STAGE_NAME} completed <<<<<<")
     
 except Exception as e:
