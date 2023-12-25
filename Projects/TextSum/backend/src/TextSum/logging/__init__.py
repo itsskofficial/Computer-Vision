edffe598aca88dfb654350aa67271186a3eb8185
SK <sarthakkarandikar03@gmail.com>
2023-12-25 17:29:30 +0530

    typo in common

--- Projects/TextSum/backend/src/TextSum/logging/__init__.py
+++ Projects/TextSum/backend/src/TextSum/logging/__init__.py
@@ -3,7 +3,7 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 log_dir = "logs"
-log_filepath = ps.path.join(log_dir, "running_logs.log")
+log_filepath = os.path.join(log_dir, "running_logs.log")
 os.makedirs(log_dir, exist_ok = True)
 
 logging.basicConfig(
